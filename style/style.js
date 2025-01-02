cdae544a7040b5a25b79950a83c15ff7d1118abd
ambrouse <thcihthichich3568@gmail.com>
2025-01-02 22:26:37 +0700

    l

--- style/style.js
+++ style/style.js
@@ -1,23 +1,23 @@
-// $(document).ready(function(){
+$(document).ready(function(){
+
     
+});
 
-
-// });
 
 const openPodup = (number)=>{
-    const podup_ = ".main .main__deriptions .main__podup:nth-child("
+    const podup_ = ".main .main__deriptions .main__podup .main__podup--item:nth-child("
 
 
     $(podup_+number+")").css("display","block")
+    $(".main .main__deriptions .main__podup").css("z-index","100")
     console.log(podup_+number+")")
 
 }
 const closePodup = (number)=>{
-    const podup_ = ".main .main__deriptions .main__podup:nth-child("
+    const podup_ = ".main .main__deriptions .main__podup .main__podup--item:nth-child("
 
-
+    $(".main .main__deriptions .main__podup").css("z-index","-1")
     $(podup_+number+")").css("display","none")
     console.log(podup_+number+")")
 
 }
-
