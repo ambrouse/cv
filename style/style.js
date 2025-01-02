56ffb83c0411754dc97c2705beff67595a572a3e
ambrouse <thcihthichich3568@gmail.com>
2025-01-02 23:39:37 +0700

    l

--- style/style.js
+++ style/style.js
@@ -1,23 +1,29 @@
-$(document).ready(function(){
+var scrollValue_;
+var check = true;
 
-    
+$(document).ready(function(){
+    $(".main .main__podup").css("max-height",$(".main")[0].scrollHeight+"px")
+    $(".main .main__podup .main__podup--item").css("max-height",$(".main")[0].scrollHeight+"px")
+    window.addEventListener("scroll", (event) => {
+        if(check==true){
+            scrollValue_ = $(window).scrollTop();
+        }
+    });
 });
 
 
 const openPodup = (number)=>{
+    check = false;
     const podup_ = ".main .main__deriptions .main__podup .main__podup--item:nth-child("
-
-
     $(podup_+number+")").css("display","block")
     $(".main .main__deriptions .main__podup").css("z-index","100")
-    console.log(podup_+number+")")
+    window.scrollTo(0, 0);
 
 }
 const closePodup = (number)=>{
+    window.scrollTo(0, scrollValue_);
+    check = true
     const podup_ = ".main .main__deriptions .main__podup .main__podup--item:nth-child("
-
     $(".main .main__deriptions .main__podup").css("z-index","-1")
     $(podup_+number+")").css("display","none")
-    console.log(podup_+number+")")
-
 }
